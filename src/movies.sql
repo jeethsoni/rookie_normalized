207204b5a0af89e1a85d578be6e2f52d7cb2c0f1
Jeet Soni <jeet.soni0822@gmail.com>
2023-06-28 14:05:27 -0400

    fixed foreign key bugs

--- src/movies.sql
+++ src/movies.sql
@@ -15,8 +15,8 @@ CREATE TABLE IF NOT EXISTS movie(
     votes INT NOT NULL,
     revenue decimal NOT NULL,
     metascore int NOT NULL,
-    primary key(movie_id),
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    primary key(movie_id)
 
 );
 
@@ -24,8 +24,8 @@ CREATE TABLE IF NOT EXISTS movie(
 CREATE TABLE IF NOT EXISTS genre(
     genre_id INT NOT NULL AUTO_INCREMENT,
     name varchar(75) NOT NULL,
-    primary key(genre_id),
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    primary key(genre_id)
 
 );
 
@@ -36,9 +36,8 @@ CREATE TABLE IF NOT EXISTS actor(
     last_name varchar(100) NOT NULL,
     gender varchar(25) NOT NULL,
     age INT NOT NULL,
-    primary key(actor_id),
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL 
-
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+    primary key(actor_id)
 );
 
 -- director table
@@ -46,9 +45,8 @@ CREATE TABLE IF NOT EXISTS director(
     director_id INT NOT NULL AUTO_INCREMENT,
     first_name varchar(100) NOT NULL,
     last_name varchar(100) NOT NULL,
-    primary key(director_id),
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
-
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    primary key(director_id)
 );
 
 -- 1 movie_id N movie_id(movie_genre)
@@ -56,10 +54,10 @@ CREATE TABLE IF NOT EXISTS director(
 CREATE TABLE IF NOT EXISTS movie_genre(
     movie_id INT NOT NULL,
     genre_id INT NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
     primary key(movie_id, genre_id),
-    FOREIGN KEY movie_id REFERENCES movie_id(movie),
-    FOREIGN KEY genre_id REFERENCES genre_id(genre),
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
+    FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
+    FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
 );
 
 -- 1 movie_id N movie_id(movie_actor)
@@ -67,10 +65,10 @@ CREATE TABLE IF NOT EXISTS movie_genre(
 CREATE TABLE IF NOT EXISTS movie_actor(
     movie_id INT NOT NULL,
     actor_id INT NOT NULL,
-    FOREIGN KEY movie_id REFERENCES movie_id(movie),
-    FOREIGN KEY actor_id REFERENCES actor_id(actor),
-    primary key(movie_id, genre_id),
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    primary key(movie_id, actor_id),
+    FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
+    FOREIGN KEY (actor_id) REFERENCES actor(actor_id)
 );
 
 -- 1 movie_id N movie_id(movie_director)
@@ -78,8 +76,8 @@ CREATE TABLE IF NOT EXISTS movie_actor(
 CREATE TABLE IF NOT EXISTS movie_director(
     movie_id INT NOT NULL,
     director_id INT NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
     primary key(movie_id, director_id),
-    FOREIGN KEY movie_id REFERENCES movie_id(movie),
-    FOREIGN KEY director_id REFERENCES director_id(director),
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
+    FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
+    FOREIGN KEY (director_id) REFERENCES director(director_id)
 );
